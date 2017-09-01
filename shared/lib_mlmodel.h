f027991409f18faffb9688a29538710009ea5ac4
Timo Richert <timo.richert@pvyield.com>
2017-09-01 11:05:17 +0200

    Changed location of ML model base files to shared folder.

--- shared/lib_mlmodel.h
+++ shared/lib_mlmodel.h
@@ -1,10 +1,10 @@
 /*******************************************************************************************************
-* 
+*
 * This code is licensed with the MIT license
 * https://opensource.org/licenses/MIT
 *
 * Copyright 2017 - pvyield GmbH / Timo Richert
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 * associated documentation files(the "Software"), to deal in the Software without restriction, including
 * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
